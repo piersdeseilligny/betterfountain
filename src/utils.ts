e5f23ac4748a39b1ce0582f1224c07bf0976d005
Rick Schubert <rickschubert@gmx.de>
2019-07-13 00:02:48 +0100

    Also match lower case scene heading markers

--- src/utils.ts
+++ src/utils.ts
@@ -61,8 +61,8 @@ const getLinePosition = (screenplayText: string, scene: string): number => {
 }
 
 export const getSceneFoldingRanges = (screenplayText: string): vscode.FoldingRange[] => {
-    const sceneHeadingRegexLookAhead = /^(?=^(?:INT|EXT|INT\/EXT|EST|I\/E|I\.\/E)(?: |\.).*$)/m
-    const sceneHeadingRegex = /^(?:INT|EXT|INT\/EXT|EST|I\/E|I\.\/E)(?: |\.).*$/
+    const sceneHeadingRegexLookAhead = /^(?=^(?:INT|EXT|INT\/EXT|EST|I\/E|I\.\/E)(?: |\.).*$)/mi
+    const sceneHeadingRegex = /^(?:INT|EXT|INT\/EXT|EST|I\/E|I\.\/E)(?: |\.).*$/i
 	const headingsLinePositions: number[] = []
 
 	// Step 1: Create an array of scenes
