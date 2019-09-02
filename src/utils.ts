16fdb688d837a94da16be5ec5a4cfaa62436892d
Rick Schubert <rickschubert@gmx.de>
2019-09-02 22:31:03 +0100

    Remove unused code

--- src/utils.ts
+++ src/utils.ts
@@ -1,17 +1,10 @@
 import * as vscode from "vscode";
-import { FountainStructureProperties } from "./extension";
-
 //var syllable = require('syllable');
 
 /**
  * Trims character extensions, for example the parantheses part in `JOE (on the radio)`
  */
 export const trimCharacterExtension = (character: string): string => character.replace(/( \([A-z0-9 '\-.()]+\))*(\s*\^*)?$/, "");
-
-/**
- * Trims the `@` symbol necesary in character names if they contain lower-case letters, i.e. `@McCONNOR`
- */
-const trimCharacterForceSymbol = (character: string): string => character.replace(/^@/, "");
 
 /**
  * Character names containing lowercase letters need to be prefixed with an `@` symbol
@@ -22,7 +15,7 @@ export const addForceSymbolToCharacter = (characterName: string): string => {
 }
 
 export const getCharactersWhoSpokeBeforeLast = (parsedDocument:any, position:vscode.Position) => {
-	
+
 	let searchIndex = 0;
 	if(parsedDocument.tokenLines[position.line-1]){
 		searchIndex = parsedDocument.tokenLines[position.line-1];
@@ -47,36 +40,6 @@ export const getCharactersWhoSpokeBeforeLast = (parsedDocument:any, position:vsc
 		searchIndex--;
 	}
 	return previousCharacters;
-}
-
-export const findCharacterThatSpokeBeforeTheLast = (
-	document: vscode.TextDocument,
-	position: vscode.Position,
-	fountainDocProps: FountainStructureProperties,
-	): string => {
-
-	const isAlreadyMentionedCharacter = (text: string): boolean => fountainDocProps.characters.has(text);
-
-	let characterBeforeLast = "";
-	let lineToInspect = 1;
-	let foundLastCharacter = false;
-	do {
-		const beginningOfLineToInspect = new vscode.Position(position.line - lineToInspect, 0);
-		const endOfLineToInspect = new vscode.Position(position.line - (lineToInspect - 1), 0);
-		let potentialCharacterLine = document.getText(new vscode.Range(beginningOfLineToInspect, endOfLineToInspect)).trimRight();
-		potentialCharacterLine = trimCharacterExtension(potentialCharacterLine);
-		potentialCharacterLine = trimCharacterForceSymbol(potentialCharacterLine);
-		if (isAlreadyMentionedCharacter(potentialCharacterLine)) {
-			if (foundLastCharacter) {
-				characterBeforeLast = potentialCharacterLine;
-			} else {
-				foundLastCharacter = true;
-			}
-		}
-		lineToInspect++;
-	} while (!characterBeforeLast);
-
-	return characterBeforeLast;
 }
 
 /**
