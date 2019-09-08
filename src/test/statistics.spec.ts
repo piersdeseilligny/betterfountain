cde155901b1182f7b485e4d00e99e18f2e8bff5c
Rick Schubert <rickschubert@gmx.de>
2019-09-08 21:35:49 +0100

    Unit test for big fish CRLF and LF carraige returns

--- src/test/statistics.spec.ts
+++ src/test/statistics.spec.ts
@@ -2,18 +2,27 @@ import { retrieveScreenPlayStatistics } from "../statistics";
 import * as fs from "fs"
 import * as path from "path"
 
+const bigFishAssertions = (bigFishScript: string) => {
+    const stats = retrieveScreenPlayStatistics(bigFishScript)
+    expect(stats.characterStats.length).toBe(42)
+    stats.characterStats.forEach((charStat) => {
+        expect(typeof charStat.name).toBe("string")
+        expect(charStat.name.length).toBeGreaterThan(0)
+        expect(charStat.speakingParts).toBeGreaterThan(0)
+        expect(charStat.wordsSpoken).toBeGreaterThan(0)
+    })
+    expect(stats.sceneStats.length).toBe(190)
+}
+
 describe("Statistics", () => {
-    it("Big Fish", () => {
-        const bigFish = fs.readFileSync(path.resolve(__dirname, "./big_fish.fountain"), "utf-8")
-        const stats = retrieveScreenPlayStatistics(bigFish)
-        expect(stats.characterStats.length).toBe(42)
-        stats.characterStats.forEach((charStat) => {
-            expect(typeof charStat.name).toBe("string")
-            expect(charStat.name.length).toBeGreaterThan(0)
-            expect(charStat.speakingParts).toBeGreaterThan(0)
-            expect(charStat.wordsSpoken).toBeGreaterThan(0)
-        })
-        expect(stats.sceneStats.length).toBe(190)
+    it("Big Fish CRLF", () => {
+        const bigFish = fs.readFileSync(path.resolve(__dirname, "./big_fish_crlf.fountain"), "utf-8")
+        bigFishAssertions(bigFish)
+    })
+
+    it("Big Fish LF", () => {
+        const bigFish = fs.readFileSync(path.resolve(__dirname, "./big_fish_lf.fountain"), "utf-8")
+        bigFishAssertions(bigFish)
     })
 
     it("Blank Canvas script", () => {
