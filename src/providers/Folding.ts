c493b5c38113b01e709a0153c0423f5d2276af4c
piersdeseilligny <piers.deseilligny+github@outlook.com>
2020-07-16 00:45:47 +0200

    Folding for sections (based on StructTokens)

--- src/providers/Folding.ts
+++ src/providers/Folding.ts
@@ -1,79 +1,33 @@
-import { FoldingRangeProvider, FoldingRange, TextDocument } from "vscode";
+import { FoldingRangeProvider, FoldingRange, TextDocument, FoldingRangeKind } from "vscode";
 import { parsedDocuments } from "../extension";
+import { StructToken } from "../afterwriting-parser";
 
-/*
-function GetSceneRanges(document: TextDocument) : FoldingRange[]{
-		var matchlines = [];
-		var ranges = [];
-		for (let index = 0; index < parsedDocument.tokens.length; index++) {
-			if(parsedDocument.tokens[index].type=="scene_heading"){
-				matchlines.push(parsedDocument.tokens[index].position);
-			}
-		}
-		for (let index = 0; index < matchlines.length; index++) {
-			if(index == matchlines.length-1)
-				ranges.push(new FoldingRange(matchlines[index], document.lineCount-1));
-			else if(matchlines[index+1]-matchlines[index] <= 1) continue;
-			else{
-				ranges.push(new FoldingRange(matchlines[index], matchlines[index+1]-1));
-			}
-		}
-		return ranges;
-}*/
-function GetFoldingRange(matchlines: number[], lineCount: number/*, higherRange?: FoldingRange[]*/): FoldingRange[] {
-	var ranges: FoldingRange[] = [];
-	for (let index = 0; index < matchlines.length; index++) {
-		if (index == matchlines.length - 1)
-			ranges.push(new FoldingRange(matchlines[index], lineCount - 1));
-		else if (matchlines[index + 1] - matchlines[index] <= 1) continue;
-		else {
-			ranges.push(new FoldingRange(matchlines[index], matchlines[index + 1] - 1));
-		}
-	}
-	return ranges;
-}
-function GetFullRanges(document: TextDocument): FoldingRange[] {
-	var h1matches = []; //#
-	var h2matches = []; //##
-	var h3matches = []; //### (or more)
-	var scenematches = []; //scene headings
-	var ranges: FoldingRange[] = [];
-	var parsedDocument = parsedDocuments.get(document.uri.toString());
-	for (let index = 0; index < parsedDocument.tokens.length; index++) {
-		if (parsedDocument.tokens[index].type == "section") {
-			var depth = parsedDocument.tokens[index].level;
-			if (depth >= 3) {
-				h3matches.push(parsedDocument.tokens[index].line);
-			}
-			else if (depth == 2) {
-				h2matches.push(parsedDocument.tokens[index].line);
-			}
-			else if (depth == 1) {
-				h1matches.push(parsedDocument.tokens[index].line);
-			}
-		}
-		else if (parsedDocument.tokens[index].type == "scene_heading") {
-			scenematches.push(parsedDocument.tokens[index].line);
-		}
-	}
-	//  TODO: Enable folding for headers:
-	//	ranges = ranges.concat(GetFoldingRange(h1matches, document.lineCount));
-	//	ranges = ranges.concat(GetFoldingRange(h2matches, document.lineCount));
-	//	ranges = ranges.concat(GetFoldingRange(h3matches, document.lineCount));
-	//	ranges = ranges.concat(GetFoldingRange(hmmatches, document.lineCount));
-	ranges = GetFoldingRange(scenematches, document.lineCount);
-	return ranges;
-}
 export class FountainFoldingRangeProvider implements FoldingRangeProvider {
 	provideFoldingRanges(document: TextDocument): FoldingRange[] {
 		var ranges: FoldingRange[] = [];
+		if (parsedDocuments.has(document.uri.toString())) {
 
-		//Get the sections
-		//ranges = ranges.concat(GetSectionRanges("section", document));
+			function addRange(structItem: StructToken, nextStructItem: StructToken, lastline:number) {
 
-		//Add the scenes
-		//ranges = ranges.concat(GetFullRanges(document));
-		ranges = GetFullRanges(document);
+				if(nextStructItem != undefined) //this is the last child, so the end of the folding range is the end of the parent
+					lastline = nextStructItem.range.start.line;
+				ranges.push(new FoldingRange(structItem.range.start.line, lastline-1, FoldingRangeKind.Region));
+
+				if(structItem.children && structItem.children.length){
+					//for each child of the structtoken, repeat this process recursively
+					for (let i = 0; i < structItem.children.length; i++) {
+						addRange(structItem.children[i], structItem.children[i + 1], lastline);
+					}
+				}
+				
+			}
+
+			let parsed = parsedDocuments.get(document.uri.toString());
+			for (let i = 0; i < parsed.properties.structure.length; i++) {
+				//for each structToken, add a new range starting on the current structToken and ending on either the next one, or the last line of the document
+				addRange(parsed.properties.structure[i], parsed.properties.structure[i+1], document.lineCount);
+			}
+		}
 		return ranges;
 	}
 }
