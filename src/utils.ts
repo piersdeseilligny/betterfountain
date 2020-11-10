c42e322c4ce73d688beffa7c1e87a2bb0bb8de09
piersdeseilligny <piers.deseilligny+github@outlook.com>
2020-11-10 22:45:00 +0000

    Fix "open" button in PDF Export notification

--- src/utils.ts
+++ src/utils.ts
@@ -161,11 +161,11 @@ export function openFile(p:string){
 	let cmd = "xdg-open"
 	switch (process.platform) { 
 		case 'darwin' : cmd = 'open'; break;
-		case 'win32' : cmd = 'start'; break;
+		case 'win32' : cmd = ''; break;
 		default : cmd = 'xdg-open';
 	}
 	var exec = require('child_process').exec;
-	exec(cmd+ ' ' + p); 
+	exec(`${cmd} "${p}"`); 
 }
 export function revealFile(p:string){
 	var cmd = "";
