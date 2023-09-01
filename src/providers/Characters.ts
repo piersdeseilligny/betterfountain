318207f4ae4dea2deb792b8f389093514ce9bc9e
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-09-01 19:36:07 +0200

    Nicely format character items

--- src/providers/Characters.ts
+++ src/providers/Characters.ts
@@ -1,49 +1,59 @@
 import * as vscode from 'vscode';
-import { activeParsedDocument, getEditor } from '../extension';
-import { CharacterTreeItem } from './yourCharacterTreeItemFile'; // You'll need to define this
+import { activeParsedDocument } from '../extension';
 
 export class FountainCharacterTreeDataProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
-	public readonly onDidChangeTreeDataEmitter: vscode.EventEmitter<vscode.TreeItem | null> =
-		new vscode.EventEmitter<vscode.TreeItem | null>();
-	public readonly onDidChangeTreeData: vscode.Event<vscode.TreeItem | null> = this.onDidChangeTreeDataEmitter.event;
+  public readonly onDidChangeTreeDataEmitter: vscode.EventEmitter<vscode.TreeItem | null> =
+    new vscode.EventEmitter<vscode.TreeItem | null>();
+  public readonly onDidChangeTreeData: vscode.Event<vscode.TreeItem | null> = this.onDidChangeTreeDataEmitter.event;
 
-	private treeRoot: CharacterTreeItem;
+  private treeRoot: CharacterTreeItem;
 
-	getTreeItem(element: vscode.TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
-		return element;
-	}
-	getChildren(element?: CharacterTreeItem): vscode.ProviderResult<any[]> {
-		if (element)
-			return element.children;
-		if (this.treeRoot && this.treeRoot.children)
-			return this.treeRoot.children;
-		else return [];
-	}
+  getTreeItem(element: vscode.TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element;
+  }
 
-	update(): void {
-		this.treeRoot = buildCharacterTree();
-		this.onDidChangeTreeDataEmitter.fire(void 0);
-	}
+  getChildren(element?: CharacterTreeItem): vscode.ProviderResult<any[]> {
+    if (element)
+      return element.children;
+    if (this.treeRoot && this.treeRoot.children)
+      return this.treeRoot.children;
+    else return [];
+  }
+
+  update(): void {
+    this.treeRoot = buildCharacterTree();
+    this.onDidChangeTreeDataEmitter.fire(void 0);
+  }
 }
 
 function buildCharacterTree(): CharacterTreeItem {
-	const characters = activeParsedDocument().properties.characters;
+  const characters = activeParsedDocument().properties.characters;
+  const root = new CharacterTreeItem("Characters", [], null);
+  root.children = [];
 
-	const root = new CharacterTreeItem("Characters", null);
-	root.children = [];
-
-	for (const [character, wordCount] of characters) {
-		const child = new CharacterTreeItem(`${character} (${wordCount} words)`, root);
-		root.children.push(child);
-	}
-
-	return root;
+  for (const [character, wordCount] of characters.entries()) {
+    const child = new CharacterTreeItem(`${character}`, wordCount, root);
+    root.children.push(child);
+  }
+  return root;
 }
 
 class CharacterTreeItem extends vscode.TreeItem {
-	children: CharacterTreeItem[] = [];
+  children: CharacterTreeItem[] = [];
+  lineNumber: number = 0;
 
-	constructor(label: string, public parent: CharacterTreeItem) {
-		super(label);
-	}
+  constructor(label: string, public scenes: number[], public parent: CharacterTreeItem) {
+    super(label);
+    if (scenes.length > 0) {
+      scenes = scenes.map(x => x - 1); // decrease needed to get correct scene number?
+      this.description = scenes.join(", ");
+      scenes = scenes.map(x => x - 1); // decrease to get correct index
+      this.lineNumber = activeParsedDocument().properties.sceneLines[scenes[0]];
+      this.command = {
+        command: 'fountain.jumpto',
+        title: '',
+        arguments: [this.lineNumber] 
+      }
+    }
+  }
 }
